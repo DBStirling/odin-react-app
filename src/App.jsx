5ec18e5337a5f704003563389d503eabccf912ee
DBStirling <davidstirling17@gmail.com>
2024-09-26 17:39:06 -0400

    todo input works

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import TodoList from "./components/TodoList"
 
 function App() {        // main react app runs this function as its called in the main.jsx
 
+  // Creating stateful todos variable which makes it user-editable
   const [todos, setTodos] = useState([
-    'go to the gym',
-    'eat more fruits and veg',
-    'pick up the kids'
+  
   ])
+
+  // Creates a new todo list array. Lists all existing elements already, then
+  // adds the newTodo which is passed through the function parameter.
+  // Sets Todos to equal the newTodoList
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
 
   return ( 
     <>
-      <TodoInput />
+      <TodoInput handleAddTodos={handleAddTodos} />
       <TodoList todos={todos} />    
     </>
   )
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,8 +1,21 @@
-export default function TodoInput() {
+import { useState } from "react"
+
+export default function TodoInput(props) {
+    const {handleAddTodos} = props
+    // Creates todoValue stateful variable and assigns empty string as default value
+    const [todoValue, setTodoValue] = useState('')
+
     return (
         <header>
-            <input placeholder="Enter todo..." />
-            <button>Add</button>
+
+            <input value={todoValue} onChange={(e)=> {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter todo..." />
+
+            <button onClick={()=> {
+                handleAddTodos(todoValue)
+            }}>Add</button>
+
         </header>
     )
 }
